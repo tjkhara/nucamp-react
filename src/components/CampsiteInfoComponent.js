d4081996bb0c5cb19c814b52f0557a82fa37ffa0
Tajeshwar <tkhara@gmail.com>
2021-11-25 11:37:49 +0530

    Created a new component for displaying the information for the campsite in a separate card. CampsiteInfoComponent.

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,9 +1,24 @@
 import React, { Component } from "react"
+import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap"
 
 export default class CampsiteInfoComponent extends Component {
+  renderCampsite = campsite => {
+    return (
+      <div className="col-md-5 m-1">
+        <Card>
+          <CardImg top src={campsite.image} alt={campsite.name} />
+          <CardBody>
+            <CardTitle>{campsite.name}</CardTitle>
+            <CardText>{campsite.description}</CardText>
+          </CardBody>
+        </Card>
+      </div>
+    )
+  }
+
   render() {
     if (this.props.campsite) {
-      return <div className="row"></div>
+      return <div className="row">{this.renderCampsite(this.props.campsite)}</div>
     }
     return <div></div>
   }
--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from "reactstrap"
+import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap"
+import CampsiteInfo from "./CampsiteInfoComponent"
 
 export default class DirectoryComponent extends Component {
   constructor(props) {
@@ -13,22 +14,6 @@ export default class DirectoryComponent extends Component {
     this.setState({
       selectedCampsite: campsite
     })
-  }
-
-  renderSelectedCampsite = campsite => {
-    if (campsite) {
-      return (
-        <Card>
-          <CardImg top src={campsite.image} alt={campsite.name} />
-          <CardBody>
-            <CardTitle>{campsite.name}</CardTitle>
-            <CardText>{campsite.description}</CardText>
-          </CardBody>
-        </Card>
-      )
-    }
-
-    return <div></div>
   }
 
   render() {
@@ -48,9 +33,7 @@ export default class DirectoryComponent extends Component {
     return (
       <div className="container">
         <div className="row">{directory}</div>
-        <div className="row">
-          <div className="col-md-5 m-1">{this.renderSelectedCampsite(this.state.selectedCampsite)}</div>
-        </div>
+        <CampsiteInfo campsite={this.state.selectedCampsite} />
       </div>
     )
   }
