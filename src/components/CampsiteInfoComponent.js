70449c7e81c05b9794e76dbfd4f88091f7e7d0b6
Tajeshwar <tkhara@gmail.com>
2021-11-26 12:04:07 +0530

    Week 3 - 1. Exercise: Presentational and Container Components done. Code was reorganized. I need to go and understand what was done.

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -45,10 +45,12 @@ export default class CampsiteInfoComponent extends Component {
   render() {
     if (this.props.campsite) {
       return (
-        <>
-          <div className="row">{this.renderCampsite(this.props.campsite)}</div>
-          <div className="row">{this.renderComments(this.props.campsite.comments)}</div>
-        </>
+        <div className="container">
+          <div className="row">
+            {this.renderCampsite(this.props.campsite)}
+            {this.renderComments(this.props.campsite.comments)}
+          </div>
+        </div>
       )
     }
     return <div></div>
--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,28 +1,14 @@
 import React, { Component } from "react"
 import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap"
-import CampsiteInfo from "./CampsiteInfoComponent"
-
 export default class DirectoryComponent extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      selectedCampsite: null
-    }
-  }
-
   // methods
-  onCampsiteSelect = campsite => {
-    this.setState({
-      selectedCampsite: campsite
-    })
-  }
 
   // render
   render() {
     const directory = this.props.campsites.map(campsite => {
       return (
         <div className="col-md-5 m-1" key={campsite.id}>
-          <Card onClick={() => this.onCampsiteSelect(campsite)}>
+          <Card onClick={() => this.props.onClick(campsite.id)}>
             <CardImg width="100%" src={campsite.image} alt={campsite.name} />
             <CardImgOverlay>
               <CardTitle>{campsite.name}</CardTitle>
@@ -35,7 +21,6 @@ export default class DirectoryComponent extends Component {
     return (
       <div className="container">
         <div className="row">{directory}</div>
-        <CampsiteInfo campsite={this.state.selectedCampsite} />
       </div>
     )
   }
